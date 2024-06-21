36e9b4aa24bd3ba41bc6f7117d0511de16fa4323
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-06-21 15:27:48 +0800

    layout: page view scroll snap y axis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,35 +5,38 @@ import { DropIcon } from "~/svgs";
 import { WaterAccumulator } from "./_components/waterAccumulator";
 import { Drinks } from "./_components/drinks";
 import { Menu } from "./_components/menu";
-
 export default function HomePage() {
   //TODO: add scroll snap
   return (
-    <main className=" h-[100dvh] overflow-y-scroll text-slate-950 transition-all">
-      <section className="flex h-[100dvh] flex-col items-center justify-between gap-12 px-4 py-8">
-        <div className="flex w-full justify-between gap-4 self-start font-extrabold tracking-tight">
-          <div>
-            <h1 className="text-6xl text-blue-600">water</h1>
-            <h1 className="flex text-3xl">
-              warden
-              <DropIcon />
-            </h1>
-            <Timer />
+    <main className=" scrollbar-hidden h-[100dvh] snap-y snap-mandatory overflow-y-scroll scroll-smooth text-slate-950 transition-all ">
+      <section className=" h-[100lvh] snap-center snap-always">
+        <div className="flex h-[100dvh] flex-col items-center justify-between gap-12 px-4 py-8 transition-all">
+          <div className="flex w-full justify-between gap-4 self-start font-extrabold tracking-tight">
+            <div>
+              <h1 className="text-6xl text-blue-600">water</h1>
+              <h1 className="flex text-3xl">
+                warden
+                <DropIcon />
+              </h1>
+              <Timer />
+            </div>
+            <Menu />
           </div>
-          <Menu />
-        </div>
 
-        <div className="h-[500px] w-full">
-          <WaterAccumulator />
+          <div className="h-[500px] w-full">
+            <WaterAccumulator />
+          </div>
+          <Drinks />
+          <p>
+            It is recommanded that you should drink water for the first 10 hours
+            of the day.
+          </p>
         </div>
-        <Drinks />
-        <p>
-          It is recommanded that you should drink water for the first 10 hours
-          of the day.
-        </p>
       </section>
-      <section className="flex h-[100dvh] flex-col items-center justify-center ">
-        <h1 className="second page bg-red-400 text-5xl">seecond page</h1>
+      <section className="h-[100lvh] snap-center snap-always">
+        <div className="flex h-[100dvh] flex-col items-center justify-center transition-all">
+          <h1 className="  bg-red-400 text-5xl">seecond page</h1>
+        </div>
       </section>
     </main>
   );
